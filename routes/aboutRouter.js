c8361a27681edd1c53d955ec2c291ed6401efcb4
Rachel Nurmi <86205748+RachelNurmi91@users.noreply.github.com>
2022-03-07 18:03:43 -0500

    Create aboutRouter.js

---
+++ routes/aboutRouter.js
@@ -0,0 +1,11 @@
+//Here we require the express router
+const express = require('express');
+//We are bringing in the function to create the aboutRouter
+const aboutRouter = express.Router();
+
+//Setting up/creating the About routes
+aboutRouter.route('/')
+.all((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+})
