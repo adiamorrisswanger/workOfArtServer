c393e47ffa42248724f3ba0e051a8d70805f8e68
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-05-23 19:07:28 -0500

    Working on download endpoint

--- routes/downloadsRouter.js
+++ routes/downloadsRouter.js
@@ -5,7 +5,7 @@ const cors = require('./cors');
 
 downloadsRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-//Maybe I want POST instead of GET to disable that automatic download...
+//Maybe I want POST instead of GET to disable that automatic download...Doesnt seem to matter
 .get(cors.cors, (req, res) => {
     //Look at stack overflow example for server and client code
     if(response.ok) {
@@ -21,8 +21,18 @@ downloadsRouter.route('/')
     } else {
             console.log('Your file could not be downloaded');
         }
-    }
-    
-)
+    })
+.post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /downloads');
+})
+.put(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /downloads');
+})
+.delete(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    res.statusCode = 403;
+    res.end('DELETE operation not supported on /downloads');
+});
 
 module.exports = downloadsRouter;
